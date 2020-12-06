b08b916b63b119eaf281881b6a0d25dab55cd6f8
Nandaja <nandaja.varma@gmail.com>
2020-12-06 14:45:21 +0100

    Fixing up Day 6

--- src/advent_of_code_202/core.clj
+++ src/advent_of_code_202/core.clj
@@ -1,15 +1,21 @@
 (ns advent-of-code-202.core)
-(require '[clojure.java [io :as io]])
+(require '[clojure.java [io :as io]]
+         '[clojure.string :as str]
+         '[clojure.set])
 
-(defn get-file-content
-  "Reads the input file into a set"
+(defn get-file-content-line-by-line
+  "Reads the input file line by line into a set"
   [filename]
     (line-seq (io/reader filename)))
+
+(defn get-file-content
+  [filename]
+  (slurp filename))
 
 (defn to-integer
   ;; converts an element to integer
   [elem]
-  (Integer/parseInt elem)
+  (Long/parseLong elem)
 )
 
 (defn to-integers
@@ -22,6 +28,69 @@
   [string char]
   (get (frequencies string) (first char) 0))
 
+(defn filter-newlines
+ [string]
+  (filter #(not (= "\n" %))  string))
+
+(defn split-string
+  [string]
+  (clojure.string/split string  #""))
+
+(defn split-string-by-new-line
+  [string]
+  (clojure.string/split string  #"\n"))
+
+(defn lists-to-sets
+  [alist]
+  (map #(into #{} %1) alist))
+
+(defn count-common-chars
+  [words]
+  (let [strings (split-string-by-new-line  words )
+        char-lists (map  split-string strings)]
+   (count (reduce clojure.set/intersection  (lists-to-sets char-lists)))))
+
+(defn count-unique-chars
+  [word]
+  (count (distinct (filter-newlines (split-string word)))))
+
+(defn split-string-by-blank-line
+  [string]
+  (str/split string #"\r?\n\r?\n"))
+
+(defn split-string-by-space
+  [string]
+  (str/split string #"(\S+)/\n|\s|\z"))
+
+(defn split-string-by-colon
+  [string]
+  (str/split string #":"))
+
+(defn split-string-by-colon-and-return-first
+  [string]
+  (first (split-string-by-colon string)))
+
+(defn split-chars-and-nums
+  [string]
+  (str/split string #"(\d*)(\D*)"))
+
+(defn is-n-digit
+  [string n]
+  (== (count string) n))
+
+(defn is-in-the-range
+  [num min max]
+  (<= min num max))
+
+(defn is-alphanumeric-6
+  [string]
+  (re-matches #"^[a-z0-9]{6}$" string))
+
+;; (defn call [this & that]
+;;   (apply (resolve (symbol this)) that))
+;; (defn call [^String nm & args]
+;;   (when-let [fun (ns-resolve *ns* (symbol nm))]
+;;     (apply fun args)))
 ; --------------- Day 1 ------------------
 (defn day-1-1
   [input]
@@ -40,11 +109,11 @@
 
 (defn solve-day-1-1
   [input-file]
-  (day-1-1 (to-integers (get-file-content input-file))))
+  (day-1-1 (to-integers (get-file-content-line-by-line input-file))))
 
 (defn solve-day-1-2
   [input-file]
-  (day-1-2 (to-integers (get-file-content input-file))))
+  (day-1-2 (to-integers (get-file-content-line-by-line input-file))))
 
 ; --------------- Day 2 ------------------
 (defn extract-policy-params
@@ -80,11 +149,11 @@
 
 (defn solve-day-2-1
   [input-file]
-  (day-2-1 (get-file-content input-file)))
+  (day-2-1 (get-file-content-line-by-line input-file)))
 
 (defn solve-day-2-2
   [input-file]
-  (day-2-2 (get-file-content input-file)))
+  (day-2-2 (get-file-content-line-by-line input-file)))
 
 ; --------------- Day 3 ------------------
 (defn count-trees
@@ -118,13 +187,173 @@
 
 (defn solve-day-3-1
   [input-file]
-  (day-3-1 (apply list (get-file-content input-file))))
+  (day-3-1 (apply list (get-file-content-line-by-line input-file))))
 
 (defn solve-day-3-2
   [input-file]
-  (day-3-2  (apply list (get-file-content input-file))))
+  (day-3-2  (apply list (get-file-content-line-by-line input-file))))
 
+; --------------- Day 4 ------------------
+
+(defn validate-byr
+  [val]
+  (and (is-n-digit val 4) (is-in-the-range (to-integer val) 1920 2002)))
+
+(defn validate-iyr
+  [val]
+  (and (is-n-digit val 4) (is-in-the-range (to-integer val) 2010 2020)))
+
+(defn validate-eyr
+  [val]
+  (and (is-n-digit val 4) (is-in-the-range (to-integer val) 2020 2030)))
+
+(defn validate-hgt
+  [val]
+  (let [contents (split-chars-and-nums val)
+        unit (last contents)
+        height (first contents)]
+    (if (= unit "cm")
+      (is-in-the-range height 150 193)
+      (do
+        (if (= unit "in")
+          (is-in-the-range height 59 76)
+          false)))))
+
+(defn validate-hcl
+  [val]
+  (and (= "#" (first val)) (is-alphanumeric-6 (rest val ) )))
+
+(def valid-eyes
+  ["amb" "blu" "brn" "gry" "grn" "hzl" "oth"])
+
+(defn validate-ecl
+  [val]
+  (contains? valid-eyes val))
+
+(defn validate-pid
+  [val]
+  (and (is-n-digit val 9)  (= \0 (first val))))
+
+(defn validate
+  [kv]
+  (case (first kv)
+    "byr" (validate-byr (last kv))
+    "iyr" (validate-iyr (last kv))
+    "eyr" (validate-eyr (last kv))
+    "hgt" (validate-hgt (last kv))
+    "hcl" (validate-hcl (last kv))
+    "ecl" (validate-ecl (last kv))
+    "pid" (validate-pid (last kv))
+    "cid" true))
+
+(def reqd-fields
+  ["byr" "iyr" "eyr" "hgt" "hcl" "ecl" "pid"])
+
+(defn get-all-keys
+  [passport]
+  (let [fields (split-string-by-space passport)]
+        (map split-string-by-colon-and-return-first  fields)))
+
+(defn get-all-key-value-pairs
+  [passport]
+  (let [fields (split-string-by-space passport)]
+    (filter #(not (empty? %) )   (map split-string-by-colon  fields))))
+
+(defn all-fields-present?
+  [passport]
+  (every? (set (get-all-keys  passport)) reqd-fields))
+
+(defn get-valid-passports-1
+  [passports]
+  (if (empty? passports)
+      0
+      (do
+        (if (all-fields-present? (first passports))
+          (+ 1 (get-valid-passports-1 (rest passports)))
+          (get-valid-passports-1 (rest passports))))))
+
+(defn call [this & that]
+  (cond
+    (string? this) (apply (resolve (symbol this)) that)
+    (fn? this)     (apply this that)
+    :else          (conj that this)))
+
+(defn check-all-valid
+  [fields]
+  (if (empty? fields)
+    true
+    (do
+      (let [kv (first fields)]
+        (println (validate kv))
+        (println kv)
+        (and (validate kv) (check-all-valid (rest fields)))))))
+
+(defn get-valid-passports-2
+  [passports]
+  (if (empty? passports)
+      0
+      (do (let [key-value-pairs (get-all-key-value-pairs (first passports))]
+            (if (and (check-all-valid key-value-pairs) (all-fields-present? (first passports)))
+                (+ 1 (get-valid-passports-2 (rest passports)))
+                (get-valid-passports-2 (rest passports)))))))
+
+(defn day-4-1
+  [input]
+  (get-valid-passports-1 (split-string-by-blank-line input)))
+
+
+(defn day-4-2
+  [input]
+  (get-valid-passports-2 (split-string-by-blank-line input) ))
+
+(defn solve-day-4-1
+  [input-file]
+  (day-4-1 (get-file-content input-file)))
+
+(defn solve-day-4-2
+  [input-file]
+  (day-4-2 (get-file-content input-file)))
+; --------------- Day 6 ------------------
+(defn merge-maps
+  [map1 map2]
+  (merge-with + map1 map2))
+
+(defn kv [bag [k v]]
+  (assoc bag k (+ (bag k 0) v)))
+
+(defn merge-matches
+  [property-map-list]
+  (reduce #(reduce kv %1 %2) {} property-map-list))
+
+(defn fine-yes-answers
+  [answers]
+  (if (empty? answers)
+    0
+  (+ (count-unique-chars (first answers)) (fine-yes-answers (rest answers)))))
+
+(defn fine-common-yes-answers
+  [answers]
+  (if (empty? answers)
+    0
+    (+ (count-common-chars (first answers)) (fine-common-yes-answers (rest answers)))))
+
+(defn day-6-1
+  [answers]
+  (fine-yes-answers (filter #(not (empty? %)) answers)))
+
+(defn day-6-2
+  [answers]
+  (fine-common-yes-answers (filter #(not (empty? %)) answers)))
+
+(defn solve-day-6-1
+  [input-file]
+  (day-6-1 (apply list (split-string-by-blank-line (get-file-content input-file)))))
+
+(defn solve-day-6-2
+  [input-file]
+  (day-6-2 (apply list (split-string-by-blank-line (get-file-content input-file)))))
 ; --------------- Entrypoint ------------------
+
 
 (defn -main [arg]
   ;; (println (solve-day-1-1 arg))
@@ -132,4 +361,8 @@
   ;; (println (solve-day-2-1 arg))
   ;; (println (solve-day-2-2 arg))
   ;; (println (solve-day-3-1 arg))
-  (println (solve-day-3-2 arg)))
+  ;; (println (solve-day-3-2 arg))
+  ;; (println (solve-day-4-1 arg)))
+  ;; (println (solve-day-4-2 arg)))
+  ;; (println (solve-day-6-1 arg)))
+  (println (solve-day-6-2 arg)))
--- test/advent_of_code_202/core_test.clj
+++ test/advent_of_code_202/core_test.clj
@@ -8,15 +8,13 @@
    366
    299
    675
-   1456]
-)
+   1456])
 
 (def test-input-day-2
   [
    "1-3 a: abcde"
    "1-3 b: cdefg"
-   "2-9 c: ccccccccc"]
-)
+   "2-9 c: ccccccccc"])
 
 (def test-input-day-3
   ["..##.........##.........##.........##.........##.........##......."
@@ -29,8 +27,40 @@
    ".#........#.#........#.#........#.#........#.#........#.#........#"
    "#.##...#...#.##...#...#.##...#...#.##...#...#.##...#...#.##...#..."
    "#...##....##...##....##...##....##...##....##...##....##...##....#"
-   ".#..#...#.#.#..#...#.#.#..#...#.#.#..#...#.#.#..#...#.#.#..#...#.#"]
-  )
+   ".#..#...#.#.#..#...#.#.#..#...#.#.#..#...#.#.#..#...#.#.#..#...#.#"])
+
+(def test-input-day-4
+  "ecl:gry pid:860033327 eyr:2020 hcl:#fffffd
+   byr:1937 iyr:2017 cid:147 hgt:183cm
+
+   iyr:2013 ecl:amb cid:350 eyr:2023 pid:028048884
+   hcl:#cfa07d byr:1929
+
+   hcl:#ae17e1 iyr:2013
+   eyr:2024
+   ecl:brn pid:760753108 byr:1931
+   hgt:179cm
+
+   hcl:#cfa07d eyr:2025 pid:166559648
+   iyr:2011 ecl:brn hgt:59in")
+
+(def test-input-day-6
+  ["abc
+
+   a
+   b
+   c
+
+   ab
+   ac
+
+   a
+   a
+   a
+   a
+
+   b"])
+
 (deftest test-day-1-1
   (testing "Day 1 1"
     (is (= #{514579} (day-1-1 test-input-day-1)))))
@@ -54,3 +84,19 @@
 (deftest test-day-3-2
   (testing "Day 3 2"
     (is (= 336 (day-3-2 test-input-day-3)))))
+
+(deftest test-day-4-1
+  (testing "Day 4 1"
+    (is (= 2 (day-4-1 test-input-day-4)))))
+
+;; (deftest test-day-4-2
+;;   (testing "Day 4 2"
+;;     (is (= 2 (day-4-2 test-input-day-4)))))
+
+(deftest test-day-6-1
+  (testing "Day 6 1"
+    (is (= 4 (day-6-1 test-input-day-6)))))
+
+(deftest test-day-6-2
+  (testing "Day 6 2"
+    (is (= 0 (day-6-2 test-input-day-6)))))
